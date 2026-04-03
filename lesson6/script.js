daad2e05503e9f3710d534b775a42eae9a45ded0
sobitxanovmuhammadali89-alt <sobitxanovmuhammadali89@gmail.com>
2026-04-03 10:37:59 +0500

    git buyuruqlari darsi

--- lesson6/script.js
+++ lesson6/script.js
@@ -20,3 +20,28 @@ function myFunction() {
     console.log("Hello World");
     console.log("This is a JavaScript function.");
 }
+
+// git init
+
+// git remote add origin https://repozitory-manzil.git
+
+// git add .
+
+// git commit -m "comentariya yoziladi"
+
+// git push origin master
+
+// ======================================================================
+
+// Qo 'shimcha sorab qolsa:
+
+// git config --global user.name "Ismingiz"
+// git config --global user.email "emailingiz"
+
+// ============================================================================
+
+// Doim yangi cod  yozsak 3 ta git buyrug'ini doim ishlatishimiz kerak:
+
+// git add
+// git commit -m "comentariya yozing"
+// git push origin master
