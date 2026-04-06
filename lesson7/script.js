131b361c565e079d77ea57be12cdb2b77ce880db
sobitxanovmuhammadali89-alt <sobitxanovmuhammadali89@gmail.com>
2026-04-06 10:33:42 +0500

    for loop

--- lesson7/script.js
+++ lesson7/script.js
@@ -1,1 +1,42 @@
-console.log("Github")
+// For loop 
+
+// start = 1
+// end = 10
+
+//    for (let raqam = 1; raqam <= 10; raqam++) {
+//        console.log(raqam);
+//    }
+//console.log("=====================================");
+//  for (let raqam = 10; raqam >= 1; raqam--) {
+//        console.log(raqam);
+//    }  //
+
+//console.log("=====================================");
+//    for (let raqam = 10; raqam >= 1; raqam-=2) {
+//        console.log(raqam);
+//    }
+//    //
+
+//console.log("=====================================");//
+
+//     // start = 1
+//    // end = 10
+//    // step = 1//
+//
+
+//for (let raqam = 1; raqam <= 10; raqam++) {
+//    if (raqam % 2 === 0) {
+//        console.log(raqam);
+//    }
+//}//
+
+//console.log("=====================================");//
+//
+
+//for (let raqam = 1; raqam <= 10; raqam++) {
+//    if (raqam % 2 === 1) {
+//        console.log(raqam);
+//    }
+//}//
+//
+//
