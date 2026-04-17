4bccfb234c13347ee00e4a1ba4383763ff7c977d
sobitxanovmuhammadali89-alt <sobitxanovmuhammadali89@gmail.com>
2026-04-17 09:59:19 +0500

    function

---
+++ lesson11/script.js
@@ -0,0 +1,62 @@
+// let a = 10;
+// let b = 20;
+// let c = 30;
+// 
+// function maxSonniTopibBer(bir, ikki, uch){
+// if(bir > ikki && bir > uch) console.log(bir, "Katta")
+// else if (ikki > bir && ikki > uch) console.log(ikki , "Katta")
+// else console.log(bir, ikki, "va" , uch, "Bular bir-biriga teng")
+// }
+// 
+// maxSonniTopibBer(a, b, c)
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// 
+// function getValue(c, a, b){
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+// 
+// getValue(b, c ,a)
+
+//  let bir = 5;
+//  let ikki = 2;
+//  let uch = 1;
+//  let tort = 2;
+//  let besh = 4;
+//  
+//  function getData(bir, ikki,uch,tort,besh) {
+//      console.log(ikki);
+//      console.log(bir);
+//      console.log(tort);
+//      console.log(uch);
+//      console.log(beshw);
+//  }
+//  getData(ikki, besh, bir, tort, uch);
+
+// let a = 2;
+// let b = 1;
+// let c = 3;
+// 
+// function getResult (b, a, c, d) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+//     console.log(d);
+// }
+// 
+// getResult(a + b, b * c, c / a, a - b + c);
+
+// RETURN - qaytarish, qaytarmoq
+
+let a = 10;
+let b = 20;
+function yigindi(raqam1, raqam2){
+    let summa = raqam1 + raqam2;
+    return summa;
+}
+
+console.log(yigindi(a, b));
